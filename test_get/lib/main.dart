bee66bc56a6c0d11c68de92fc3e29bc444d45d67
7hmedosama <ahmedosama.202201576@gmail.com>
2025-10-19 01:07:03 +0300

    jvdjfvdjfvj

--- test_get/lib/main.dart
+++ test_get/lib/main.dart
@@ -104,7 +104,7 @@ class _MyHomePageState extends State<MyHomePage> {
           // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text('You '),
+            const Text('You  habe        pushed the button this many times:'),
             Text(
               '$_counter',
               style: Theme.of(context).textTheme.headlineMedium,
